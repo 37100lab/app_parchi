c2a2c1acd53d0351c60cfac2713e6cfcd3b8efcd
xXAleXPolYXx <ale98sayan@gmail.com>
2021-08-19 09:37:24 +0200

    add WFS

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -44,8 +44,9 @@ import quartieri from '../../data/quartieri.json'
 import sponsor from '../../assets/img/sponsor.jpg'
 import { locateSharp } from 'ionicons/icons'
 
-const url='http://3.142.202.105:7484'
-
+//const url='http://3.142.202.105:7484'
+//const url='http://localhost:8080/geoserver/geoapp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoapp%3Aparchi_poligoni&outputFormat=application%2Fjson'
+const url='http://ec2-3-142-202-105.us-east-2.compute.amazonaws.com:8080/geoserver/geoapp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoapp:parchi_poligoni&outputFormat=application/json'
 export class Map extends Component {
   
   state = {
@@ -77,7 +78,8 @@ try{
   }
 
   GetParchiPoligoni(){
-    fetch(url+'/get/parchiPoligoni', {
+    //fetch(url+'/get/parchiPoligoni', {
+      fetch(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
