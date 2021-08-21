ccd7bd3c0f54f46095f07fcd609ffecfbd0982f0
ahmedaidev <52345388+ahmedaidev@users.noreply.github.com>
2021-08-21 22:05:05 +0200

    MAINT: Deploy configs & actions

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -31,7 +31,7 @@ import {
   LayersControl,
 } from 'react-leaflet'
 
-import {dismissLocationModal} from '../../redux/actions'
+import { dismissLocationModal } from '../../redux/actions'
 import classes from './Map.module.css'
 
 import LocationMarkers from '../../components/location/LocationMarkers'
@@ -43,32 +43,29 @@ import quartieri from '../../data/quartieri.json'
 
 import sponsor from '../../assets/img/sponsor.jpg'
 import { locateSharp } from 'ionicons/icons'
+import { url } from '../../config/config'
 
-//const url='http://3.142.202.105:7484'
-//const url='http://localhost:8080/geoserver/geoapp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoapp%3Aparchi_poligoni&outputFormat=application%2Fjson'
-const url='http://ec2-3-142-202-105.us-east-2.compute.amazonaws.com:8080/geoserver/geoapp/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geoapp:parchi_poligoni&outputFormat=application/json'
 export class Map extends Component {
-  
   state = {
     mapContainer: false,
-    parchiPoligoni:{},
-    quartieri:{},
+    parchiPoligoni: {},
+    quartieri: {},
     circoscrizioni: {},
-    center:[45.438351, 10.99171],
-    mapCont:null,
-    gpsError:false,
+    center: [45.438351, 10.99171],
+    mapCont: null,
+    gpsError: false,
   }
 
   componentDidCatch() {
-    this.setState({gpsError:true})
+    this.setState({ gpsError: true })
   }
   async componentDidMount() {
-try{
-    const res = await Geolocation.getCurrentPosition()
-    this.center=[res.coords.latitude, res.coords.longitude]
-  } catch (e) {
-    this.setState({ gpsError: true })
-}
+    try {
+      const res = await Geolocation.getCurrentPosition()
+      this.center = [res.coords.latitude, res.coords.longitude]
+    } catch (e) {
+      this.setState({ gpsError: true })
+    }
     this.GetParchiPoligoni()
     if (this.state.mapContainer) return
 
@@ -77,41 +74,46 @@ try{
     }, 500)
   }
 
-  GetParchiPoligoni(){
+  GetParchiPoligoni() {
     //fetch(url+'/get/parchiPoligoni', {
-      fetch(url, {
+    fetch(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-      }
+      },
     })
-    .then(response => response.json())
-    .then(data => {
-      var appoggio = 0
-      for (let j=0; j<data.features.length; j++){
-        for (let i = 0; i<data.features[j].geometry.coordinates[0][0].length; i++){
-          appoggio = data.features[j].geometry.coordinates[0][0][i][1]
-          data.features[j].geometry.coordinates[0][0][i][1] = data.features[j].geometry.coordinates[0][0][i][0]
-          data.features[j].geometry.coordinates[0][0][i][0] = appoggio
+      .then(response => response.json())
+      .then(data => {
+        var appoggio = 0
+        for (let j = 0; j < data.features.length; j++) {
+          for (
+            let i = 0;
+            i < data.features[j].geometry.coordinates[0][0].length;
+            i++
+          ) {
+            appoggio = data.features[j].geometry.coordinates[0][0][i][1]
+            data.features[j].geometry.coordinates[0][0][i][1] =
+              data.features[j].geometry.coordinates[0][0][i][0]
+            data.features[j].geometry.coordinates[0][0][i][0] = appoggio
+          }
         }
-    }
-      this.setState({parchiPoligoni : data})
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+        this.setState({ parchiPoligoni: data })
+      })
+      .catch(error => {
+        console.error('Error:', error)
+      })
   }
 
   //ON EACH METHODS
-  OnEachParcoPoligono = (parcoPoligono, layer) =>{
+  OnEachParcoPoligono = (parcoPoligono, layer) => {
     layer.bindPopup(parcoPoligono.properties.denominazi)
   }
 
-  OnEachQuartiere = (quartiere, layer) =>{
+  OnEachQuartiere = (quartiere, layer) => {
     layer.bindPopup(stringManager.titleCase(quartiere.properties.quartiere))
   }
 
-  OnEachCircoscrizione = (paese, layer) =>{
+  OnEachCircoscrizione = (paese, layer) => {
     layer.bindPopup(stringManager.titleCase(paese.properties.circoscriz))
   }
 
@@ -119,131 +121,134 @@ try{
     const { zoom, locationClicked, showModal } = this.props.map
     const centerPosition = () => {
       console.log(this.center)
-      if(this.center)
-        this.state.mapCont.flyTo(this.center)
-      if(typeof this.center==='undefined')
-        this.setState({ gpsError: true })
+      if (this.center) this.state.mapCont.flyTo(this.center)
+      if (typeof this.center === 'undefined') this.setState({ gpsError: true })
     }
-    if(this.state.gpsError)
+    if (this.state.gpsError)
       return (
         <IonPage>
           <IonHeader>
             <IonToolbar>
-            <IonTitle>Parchi a Verona</IonTitle>
+              <IonTitle>Parchi a Verona</IonTitle>
             </IonToolbar>
-            </IonHeader>
-            <IonContent>
+          </IonHeader>
+          <IonContent>
             <IonList>
-            <IonItem>
-            <IonLabel className="ion-text-wrap">Errore nell'avvio dell'applicazione</IonLabel>
-            </IonItem>
-            <IonItem>
-            <IonLabel className="ion-text-wrap">Assicurarsi che il Geolocalizzazione 
-            e la connessione internet siano attive</IonLabel>
-            </IonItem>
+              <IonItem>
+                <IonLabel className="ion-text-wrap">
+                  Errore nell'avvio dell'applicazione
+                </IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel className="ion-text-wrap">
+                  Assicurarsi che il Geolocalizzazione e la connessione internet
+                  siano attive
+                </IonLabel>
+              </IonItem>
             </IonList>
           </IonContent>
         </IonPage>
       )
     else
+      return (
+        <IonPage>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Parchi a Verona</IonTitle>
+            </IonToolbar>
+          </IonHeader>
 
+          <IonContent id="content" fullscreen>
+            <IonModal isOpen={showModal} backdropDismiss={false}>
+              {locationClicked && <LocationModal loc={locationClicked} />}
+              <IonButton onClick={() => this.props.dismissLocationModal()}>
+                Chiudi
+              </IonButton>
+            </IonModal>
 
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Parchi a Verona</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+            {this.state.mapContainer && (
+              <MapContainer
+                className={classes.mapContainer}
+                center={this.center}
+                zoom={zoom}
+                whenCreated={mapCont => this.setState({ mapCont })}
+              >
+                <LayersControl position="topright">
+                  <LayersControl.BaseLayer checked name="Mappa base">
+                    <TileLayer
+                      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                  </LayersControl.BaseLayer>
+                  <LayersControl.BaseLayer name="Circoscrizioni">
+                    <GeoJSON
+                      key="circoscrizioni"
+                      data={circoscrizioni.features}
+                      onEachFeature={this.OnEachCircoscrizione}
+                    />
+                  </LayersControl.BaseLayer>
+                  <LayersControl.BaseLayer name="Quartieri">
+                    <GeoJSON
+                      key="quartieri"
+                      data={quartieri.features}
+                      onEachFeature={this.OnEachQuartiere}
+                    />
+                  </LayersControl.BaseLayer>
+                </LayersControl>
 
-        <IonContent id="content" fullscreen>
-
-
-          <IonModal isOpen={showModal} backdropDismiss={false}>   
-            { locationClicked && ( <LocationModal loc={locationClicked}/> )}
-            <IonButton onClick={() => this.props.dismissLocationModal()}>
-              Chiudi
-            </IonButton>
-          </IonModal>
-
-          {this.state.mapContainer && (
-            <MapContainer
-              className={classes.mapContainer}
-              center={this.center}
-              zoom={zoom}
-              whenCreated={mapCont => this.setState({ mapCont })}
-            >
-
-            <LayersControl position="topright">
-              <LayersControl.BaseLayer checked name="Mappa base">
                 <TileLayer
                   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-              </LayersControl.BaseLayer>   
-              <LayersControl.BaseLayer name="Circoscrizioni">
-                <GeoJSON key='circoscrizioni' data={circoscrizioni.features} onEachFeature={this.OnEachCircoscrizione} />
-              </LayersControl.BaseLayer>      
-              <LayersControl.BaseLayer name="Quartieri">
-                <GeoJSON key='quartieri' data={quartieri.features} onEachFeature={this.OnEachQuartiere} />
-              </LayersControl.BaseLayer>
-            </LayersControl> 
+                <MapConsumer>
+                  {map => {
+                    map.setView(this.center)
+                    return null
+                  }}
+                </MapConsumer>
+                <Marker position={this.center}>
+                  <Popup>Tu sei qui</Popup>
+                </Marker>
+                <LocationMarkers myloc={this.state.parchiPoligoni.features} />
+              </MapContainer>
+            )}
 
+            <IonFab vertical="bottom" horizontal="end" slot="fixed">
+              <IonFabButton onClick={() => centerPosition()}>
+                <IonIcon icon={locateSharp} />
+              </IonFabButton>
+            </IonFab>
+          </IonContent>
 
-              <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <MapConsumer>
-                {map => {
-                  map.setView(this.center)
-                  return null
-                }}
-              </MapConsumer>
-              <Marker position={this.center}>
-                <Popup>Tu sei qui</Popup>
-              </Marker>
-              <LocationMarkers myloc={this.state.parchiPoligoni.features}/>
-            </MapContainer>
-          )}
-
-          <IonFab vertical="bottom" horizontal="end" slot="fixed">
-            <IonFabButton onClick={() => centerPosition()}>
-              <IonIcon icon={locateSharp} />
-            </IonFabButton>
-          </IonFab>
-        </IonContent>
-
-        <IonFooter>
-            <IonImg src={sponsor}
-            style={{maxWidth: "500px" , margin: "auto"}}
+          <IonFooter>
+            <IonImg
+              src={sponsor}
+              style={{ maxWidth: '500px', margin: 'auto' }}
             />
-        </IonFooter>
-        <IonToast
-        isOpen={this.state.gpsError}
-        color="danger"
-        onDidDismiss={() => 
-          this.setState({ gpsError: false })}
-        message="Problema di caricamento mappa. Il GPS è attivo?"
-        buttons={[
-          {
-            text: 'OK',
-            role: 'cancel',
-            handler: () => {
-              this.setState({ gpsError: false })
-            }
-          }
-        ]}
-      />
-
-      </IonPage>
-    )
+          </IonFooter>
+          <IonToast
+            isOpen={this.state.gpsError}
+            color="danger"
+            onDidDismiss={() => this.setState({ gpsError: false })}
+            message="Problema di caricamento mappa. Il GPS è attivo?"
+            buttons={[
+              {
+                text: 'OK',
+                role: 'cancel',
+                handler: () => {
+                  this.setState({ gpsError: false })
+                },
+              },
+            ]}
+          />
+        </IonPage>
+      )
   }
 }
 const mapStateToProps = state => ({
   map: state.map,
 })
 
-const mapDispatchToProps = {dismissLocationModal}
+const mapDispatchToProps = { dismissLocationModal }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Map)
